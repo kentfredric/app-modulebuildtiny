c8ab03a397461e2e5682982c1126988ffc5b8e62
Leon Timmermans <fawaka@gmail.com>
2015-04-04 21:45:53 +0200

    Added minter

--- lib/App/ModuleBuildTiny.pm
+++ lib/App/ModuleBuildTiny.pm
@@ -288,6 +288,11 @@ my %actions = (
 		}
 		return 0;
 	},
+	mint => sub {
+		my @arguments = @_;
+		require App::ModuleBuildTiny::Mint;
+		return App::ModuleBuildTiny::Mint::mint_modulebuildtiny(@arguments);
+	},
 );
 
 sub modulebuildtiny {
